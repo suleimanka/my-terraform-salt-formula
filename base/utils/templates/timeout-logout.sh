b54628ac8337bb64547c149bdbe955f04a32f688
Sulejman Husaenov <sulejman.husaenov@most-finance.com>
2025-06-23 15:31:18 +0300

    Delete base directory

--- base/utils/templates/timeout-logout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{%- from 'base/settings.jinja' import base with context %}
-# {{ salt['pillar.get']('salt_banner','Managed by salt') }}
-
-TMOUT={{ base.shell.timeout }}
-readonly TMOUT
-export TMOUT
